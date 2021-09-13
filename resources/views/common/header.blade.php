4feda3d6c8447ab79211245195f5132407c0d5a7
Crystal-force <ballcryst@yandex.com>
2021-09-13 13:36:44 -0700

    Website header fixed

---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,21 @@
+<section>
+  <div>
+    <div class="cover-image sptb-1 bg-background" data-image-src="../assets/images/banners/banner1.jpg">
+      <div class="header-text1 mb-0">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-8 col-lg-8 col-md-12">
+              <div class="text-white mt-lg-7 mb-5">
+                <h1 class="mb-3 display-3"><span class="font-weight-bold">Expertos Chile</span><br> is professional<br> scientific community</h1>
+                <p>Design, plan, modify, organize, execute, develop, impart and evaluate training activities, courses, advice, couching and mentoring at a professional, scientific and / or technological level for institutions and companies of all types and sizes, as well as natural persons</p>
+                <a href="#" class="btn btn-primary btn-lg mr-2">Course</a>
+                <a href="#" class="btn btn-success btn-lg">Create your free account</a>
+              </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-12"></div>
+          </div>
+        </div>
+      </div><!-- /header-text -->
+    </div>
+  </div>
+</section>
